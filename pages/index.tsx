6b4761d2c9ef3e9c61f44ea13d3128f0c4cba36d
emiliano <emilianobarea8@gmail.com>
2022-08-01 13:03:31 -0300

    agregados

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,10 @@ const Home: NextPage = () => {
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-none">
       <Head>
         <title>Instagram Clone</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+        />
       </Head>
       <Modal />
       <Header />
